69bdb0cebbb5c4677c662a681b872dcada8e18b8
Javier <Jay14539@gmail.com>
2023-10-09 14:49:20 -0700

    event listener

--- src/script.js
+++ src/script.js
@@ -2,4 +2,14 @@ const searchButton = document.getElementById('searchButton');
 const pokemonNameInput = document.getElementById('pokemonName');
 const pokemonInfo = document.getElementById('pokemonInfo');
 
+searchButton.addEventListener('click', () => {
+    const pokemonName = pokemonNameInput.value.toLowerCase();
+    if (pokemonName) {
+        fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`)
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error('Pokemon not found');
+                }
+                return response.json();
+            })
 
