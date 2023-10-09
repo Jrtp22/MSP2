83727dcd5c1955b77f39a6d6accfaf6a1db997d4
Jose Torres <jrtp22@gmail.com>
2023-10-09 16:47:10 -0700

    added Neslys File

---
+++ poke-slay/src/Components/OnePoke.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Pokemon from '.App.js';
+
+const App = () => {
+  return (
+    <div>
+      <h1>Pokemon Information</h1>
+      <Pokemon wartortle="wartortle" /> {}
+    </div>
+  );
+};
+
+export default App;
