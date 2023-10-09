2e6c90600b251004ed323952cfd20fa6f66c7a8c
Javier <Jay14539@gmail.com>
2023-10-09 14:48:42 -0700

    basic info

---
+++ src/script.js
@@ -0,0 +1,5 @@
+const searchButton = document.getElementById('searchButton');
+const pokemonNameInput = document.getElementById('pokemonName');
+const pokemonInfo = document.getElementById('pokemonInfo');
+
+
